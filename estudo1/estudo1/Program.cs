ee8809720dd8ca820b0edf1faaa300cd64607be2
Alberto Barbosa 021 <85910024+Alberto21-boop@users.noreply.github.com>
2022-01-11 13:47:29 -0300

    Add files via upload

---
+++ estudo1/estudo1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace estudo1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a = 456465;
+            int b = 45;
+
+            int resultado = a * b;
+
+            Console.WriteLine("O resultado da mutiplicação é " + resultado);
+        }
+    }
+}
